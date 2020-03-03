55a24e01ae486e1487812c3115e074dd410c8616
h-mineta <h-mineta@0nyx.net>
2020-03-03 13:22:51 +0900

    [fix] py2修正漏れ

--- generate_htaccess_block4tor_py2.sh
+++ generate_htaccess_block4tor_py2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./generate_htaccess_block4tor_py3.py -p 443 -e /var/www/html/ `dig @8.8.8.8 0nyx.net +short`
+./generate_htaccess_block4tor_py2.py -p 443 -e /var/www/html/ `dig @8.8.8.8 0nyx.net +short`
